0a358288632d24e26f06790bbbf47b31d984d8b2
DAW118 <DAW118@RED16-09.iesmhp.local>
2016-12-07 16:00:42 +0100

    clase "Casa" añadida

---
+++ MiRepositorio_1/src/mirepositorio_1/Casa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mirepositorio_1;
+
+/**
+ *
+ * @author DAW118
+ */
+public class Casa {
+    String nombre;
+
+    public Casa(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
